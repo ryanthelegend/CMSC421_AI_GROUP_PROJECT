aff651af88936f94b98a6fe24807c06b439848cb
Hamad <hamad1@terpmail.umd.edu>
2024-03-02 15:51:26 -0500

    adding tutorial link

--- NodeReact_Backend/server/server.js
+++ NodeReact_Backend/server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users" : ["userOne", "userTwo", "userThree"]})
+    res.json({"users" : ["Document Summary", "URL Summary", "data3", "new data"]})
 })
 
 app.listen(5000, () => {console.log("server started on port 5000")})

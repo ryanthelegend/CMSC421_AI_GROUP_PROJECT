ff9fd4cec65e97192744cb4c4bc831e8c42df14e
HamadAlrashid <121236530+HamadAlrashid@users.noreply.github.com>
2024-04-06 16:26:45 -0400

    Delete NodeReact_Backend/server directory

--- NodeReact_Backend/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/api", (req, res) => {
-    res.json({"users" : ["Document Summary", "URL Summary", "data3", "new data"]})
-})
-
-app.listen(5000, () => {console.log("server started on port 5000")})

4898a6f70f847b0e6e1d8ad3df208d11773cca6f
HamadAlrashid <121236530+HamadAlrashid@users.noreply.github.com>
2024-05-09 20:48:18 -0400

    latest eval code

--- backend/testing_code.py
+++ backend/testing_code.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, QuantoConfig
@@ -5,9 +6,7 @@ import os
 import time
 
 gpu = True if torch.cuda.is_available() else False
-
 device = torch.device("cuda" if (gpu == True) else "cpu")
-
 token = os.getenv('TOKEN')  # Huggingface token
 
 
@@ -21,14 +20,13 @@ def min_max_scaling(data):
         return []
 
 
-
 def main():
     prompt = "Summarize the following text:\n In electrical engineering, a transformer is a passive component that transfers electrical energy from one electrical circuit to another circuit, or multiple circuits. A varying current in any coil of the transformer produces a varying magnetic flux in the transformer's core, which induces a varying electromotive force (EMF) across any other coils wound around the same core. Electrical energy can be transferred between separate coils without a metallic (conductive) connection between the two circuits. Faraday's law of induction, discovered in 1831, describes the induced voltage effect in any coil due to a changing magnetic flux encircled by the coil."
 
-    settings = ["int8", "int4", "none"]
+    settings = ["int8, int4, none"]
     speed_results = {}
     response_results = {}
-
+    all_results = {}
     for setting in settings:
         print(f"Testing quantization setting {setting}")
 
@@ -43,18 +41,24 @@ def main():
             model.to(device)
             tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=token)
             # tokenizer.to(device)
-            start_time = time.time()
 
-            inputs = tokenizer(prompt, return_tensors='pt').to(device)
-            output = model.generate(**inputs, max_new_tokens=50)
-            response = tokenizer.decode(output[0], skip_special_tokens=True)
+            all_results[setting] = np.array([])
+            for i in range(30):
+                start_time = time.time()
+                inputs = tokenizer(prompt, return_tensors='pt').to(device)
+                output = model.generate(**inputs, max_new_tokens=50)
+                response = tokenizer.decode(output[0], skip_special_tokens=True)
+                end_time = time.time()
+                execution_time = (end_time - start_time)
+                print(execution_time)
+                np.append(all_results[setting], execution_time)
 
-            end_time = time.time()
-            execution_time = end_time - start_time
-
-            speed_results[setting] = execution_time
+            speed_results[setting] = np.average(all_results[setting])
             response_results[setting] = response
-            print(f"Quantization setting {setting} took {execution_time} seconds")
+
+            print(f"Average Results: {speed_results[setting]}")
+            print(f"Standard Deviation: {np.std(speed_results[setting])}")
+            print(f"Quantization setting {setting} took {speed_results[setting]} seconds")
             print(f"Response: {response}")
         except Exception as e:
             print(f"An error occurred for quantization setting {setting}: {e.with_traceback()}")
